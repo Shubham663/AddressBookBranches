f4ea80415ee45188f1419dcc9354e95fde9c0217
unknown <mittalshubham05@gmail.com>
2020-10-01 11:35:56 +0530

    ÄddressBook class created

---
+++ src/main/java/bridgelabz/AddressBookBranches/AddressBookMain.java
@@ -0,0 +1,17 @@
+package bridgelabz.AddressBookBranches;
+
+/**
+ * Hello world!
+ *
+ */
+/**
+ * @author Shubham , class AddressBookMain for keeping  containing contact details.
+ *
+ */
+public class AddressBookMain 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to Address Book Program" );
+    }
+}
