5838903ef099d7f48dad04697f24d8bc5531b16d
unknown <mittalshubham05@gmail.com>
2020-11-05 09:48:31 +0530

    Add: added functionality for writing contact to json-server

--- src/test/java/bridgelabz/AddressBookBranches/AddressbookJsonTest.java
+++ src/test/java/bridgelabz/AddressBookBranches/AddressbookJsonTest.java
@@ -7,6 +7,7 @@ import org.hamcrest.Matchers;
 import org.junit.Test;
 
 import com.bridgelabz.AddressBookBranches.AddressBookJsonService;
+import com.bridgelabz.AddressBookBranches.ContactDetails;
 
 import io.restassured.response.Response;
 
@@ -21,10 +22,19 @@ public class AddressbookJsonTest {
 		AddressBookJsonService addressBookJsonService = AddressBookJsonService.getInstance();
 		Response response = addressBookJsonService.getList();
 		String responseAsString = response.asString();
-		System.out.println(responseAsString);
 		response.then().body("first_name", Matchers.hasItems("Lisa"));
 		response.then().body("city", Matchers.hasItems("Jhajjar"));
 		response.then().body("zip",Matchers.hasItem(12));
 	}
 	 
+	@Test
+	public void writeContactTest() {
+		ContactDetails contactDetails = new ContactDetails("Aamir","Khan", "302", "Rohtak", "Haryana", 110038, "9876983450", "sk@gmail.com");
+		AddressBookJsonService addressBookJsonService = AddressBookJsonService.getInstance();
+		Response response = addressBookJsonService.addContactToServer(contactDetails);
+		String responseAsString = response.asString();
+		response.then().body("first_name", Matchers.is("Aamir"));
+		response.then().body("city", Matchers.is("Rohtak"));
+		response.then().body("zip",Matchers.is(110038));
+	}
 }
